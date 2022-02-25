47b6ed22c1439b1957c73b342eb5d67fcab0ef4b
ToniIvars <toniivaju@gmail.com>
2022-02-25 20:58:39 +0100

    Add remove task frontend

--- main.py
+++ main.py
@@ -5,28 +5,41 @@ from kivy.uix.label import Label
 from kivy.core.window import Window
 from kivy.core.text import LabelBase
 
+# Register Noto Nerd Font
 LabelBase.register(
     name = 'Noto',
     fn_regular = 'fonts/Noto Sans Display Regular Nerd Font Complete.ttf',
     fn_bold = 'fonts/Noto Sans Medium Nerd Font Complete.ttf',
 )
 
-from kivy.uix.button import Button
-
 class CheckButton(Button):
-    def click(self):
-        print(self.id)
+    def remove_task(self):
+        # Get the label of the task
+        task_label = main_layout.ids.get(f'task_{self.task_btn_id}')
+
+        # Get the button of the task
+        task_btn = self
+
+        # Remove the task from the GridLayout
+        # main_layout.ids.tasks_layout.remove_widget(task_btn)
+        # main_layout.ids.tasks_layout.remove_widget(task_label)
+
+        # Strike the label text and change the color
+        task_label.color = 0.424, 0.459, 0.49, 1
+        task_label.text = f'[s]{task_label.text}[/s]'
 
 class TaskLabel(Label):
     pass
 
 class MainLayout(Widget):
     def add_task(self):
-        print(f'Btn pressed')
+        pass
 
 class TasklistApp(App):
     def build(self):
         global main_layout
+
+        # Make dark background
         Window.clearcolor = (0.204, 0.227, 0.251, 1)
 
         main_layout = MainLayout()
